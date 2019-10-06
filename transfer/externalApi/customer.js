3dc67d2f33823f38e07b803a87cfe8c3b79b3d4c
RajneeshYadav <36070833+RajneeshEr@users.noreply.github.com>
2019-10-06 08:57:13 +0530

    updated

--- transfer/externalApi/customer.js
+++ transfer/externalApi/customer.js
@@ -1,41 +1,22 @@
 const fastify = require('../app')
 const axios = require('axios')
 const msg = require('../config/constact')
-//var customerFlag = false
+var customerFlag = false
 
-async function getInfo(){
-try {
-    var custInfo = await axios.get('http://localhost:3002/customer/getbyId',{
+exports.CustomerApi = async (request, response) =>{
+    await axios.get('http://localhost:3002/customer/getbyId',{
         params: {
             phoneNumber : "234234233432"
           }
     })
-    return custInfo.data
-    //console.log(custInfo.data)
-} catch (error) {
-    
+    .then((apiresponse)=>{
+        if(apiresponse.status == 200){
+            return apiresponse.data
+        }else{
+            return false
+        }
+    }).catch((error)=> {
+        fastify.log.error(error)
+    })
 }
 
-    // await axios.get('http://localhost:3002/customer/getbyId',{
-    //     params: {
-    //         phoneNumber : "234234233432"
-    //       }
-    // })
-    // .then((apiresponse)=>{
-    //     if(apiresponse.status == 200){
-    //         console.log(apiresponse.data)
-    //         return apiresponse.data
-    //     }else{
-    //         response.status(200).send({
-    //             customerFlag : false,
-    //             msg : msg.NOT_FIND
-    //         })
-    //     }
-    // }).catch((error)=> {
-    //     fastify.log.error(error)
-    // })
-}
-
-var objectinfo = getInfo()
-
-console .log('custlkjlj lk : '+objectinfo)
