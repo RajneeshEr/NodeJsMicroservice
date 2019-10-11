6d94da9d72a512e5e449131e72d8e9d81bf04ce4
muhafsal <muhafsal@publicisgroupe.net>
2019-10-11 11:33:06 +0530

    API gateway has added

--- API-Gateway/app.js
+++
@@ -1,8 +0,0 @@
-// This is configuration file which is responsible for creating fastify instance
-
-const fastify = require('fastify')({
-    logger: true,
-    ignoreTrailingSlash: true // string used to determine how to handle passing / as a route with a prefix.
-})
-
-module.exports=fastify
--- API-Gateway/server.js
+++ API-Gateway/server.js
@@ -1,16 +1,6 @@
-const fastify = require('./app')
-var port = process.env.PORT || 3000;
+const path = require('path');
+const gateway = require('express-gateway');
 
-//Registering DB, Routes
-fastify.register(require('./db'))
-fastify.register(require('./routes/config'))
-fastify.register(require('./routes/customer'))
-fastify.register(require('./routes/gateway'))
-
-fastify.listen(port, async (error, address) => {
-    if(error){
-        fastify.log.error(error)
-        process.exit(1)
-    }
-    fastify.log.info(`The server started on ${address}`)
-})
+gateway()
+  .load(path.join(__dirname, 'config'))
+  .run();
